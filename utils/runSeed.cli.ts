50484aa677c7f59a47ddb3ffac205c77bf3e7665
canikwe <c.anikwe@gmail.com>
2020-12-31 05:42:58 -0500

    ✨ seeder cli is reading from the seeder directory

--- utils/runSeed.cli.ts
+++ utils/runSeed.cli.ts
@@ -1,18 +1,37 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
-import userSeeds from '../seeds/1609382027049-User'
-import { getConnection } from "typeorm";
+import userSeeds from '../seeds/1609382027049-User';
+import { Connection, getConnection } from 'typeorm';
+const fs = require('fs');
+const path = require('path');
+const seedsFolder = './seeds/';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   try {
-    const connection = getConnection()
-    const items = await userSeeds.up(connection)
-    console.log(`Successfully created ${items} seeds!`)
-    app.close()
+    const connection = getConnection();
+
+    await run(connection);
+
+    app.close();
   } catch (error) {
-    console.error('OH NOES!', error)
-    app.close()
+    console.error('OH NOES!', error);
+    app.close();
   }
 }
+
+const run = async (connection: Connection) => {
+  const fileNames = fs.readdirSync(seedsFolder);
+
+  for (const file of fileNames) {
+    const fileName: string = path.parse(file).name;
+    const seeder = require('.' + seedsFolder + fileName)?.default;
+    await runUpSeeders(seeder, connection);
+  }
+};
+
+const runUpSeeders = async (seeder, connection: Connection) => {
+  const insertNum = await seeder.up(connection);
+  console.log(`✅ Successfully inserted ${insertNum} seeds.`);
+};
 bootstrap();
