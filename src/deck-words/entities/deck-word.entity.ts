4ee66f51d03b13e71cc3a1dcc047031e7e4132d4
canikwe <c.anikwe@gmail.com>
2021-01-19 02:07:01 -0500

    🌱 deckWord table seeded

--- src/deck-words/entities/deck-word.entity.ts
+++ src/deck-words/entities/deck-word.entity.ts
@@ -11,7 +11,7 @@ export class DeckWord extends CommonEntity {
   @Column()
   starred: boolean;
 
-  @Field()
+  @Field((type) => Int)
   @Column()
   order: number;
 
--- src/word/entities/word.entity.ts
+++ src/word/entities/word.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { CommonEntity } from 'src/_shared/entities/common.entity';
 import { DeckWord } from 'src/deck-words/entities/deck-word.entity';
