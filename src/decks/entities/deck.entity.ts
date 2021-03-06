d6fe1806590626f658da4a67782fb44aa036bfe4
canikwe <c.anikwe@gmail.com>
2021-03-06 11:01:19 -0500

    🛠 refactor file structure and update prisma schema

--- src/decks/entities/deck.entity.ts
+++ src/decks/entities/deck.entity.ts
@@ -5,25 +5,19 @@ import { CommonEntity } from 'src/_base/entities/common.entity';
 import { DeckWord } from 'src/deck-words/entities/deck-word.entity';
 
 @ObjectType()
-@Entity('Decks')
 export class Deck extends CommonEntity {
   @Field()
-  @Column()
   title: string;
 
   @Field()
-  @Column()
   isPublic: boolean;
 
   @Field((type) => User)
-  @ManyToOne((type) => User, (user) => user.decks)
   user: User;
 
   @Field((type) => Int)
-  @Column('int')
   userId: number;
 
   @Field((type) => [DeckWord])
-  @OneToMany(() => DeckWord, (deckWord) => deckWord.deck)
   deckWords: DeckWord[];
 }
