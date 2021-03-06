cd1d248cb58fc4b1f0865dda8fb1a20d59b6945d
canikwe <c.anikwe@gmail.com>
2021-03-06 10:54:03 -0500

    🧪 additional findOne e2e test for users

--- src/users/e2e-tests/user.e2e-spec.ts
+++ src/users/e2e-tests/user.e2e-spec.ts
@@ -3,10 +3,11 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from '../../app.module';
 import { gql } from 'apollo-server-express';
 import { GqlTestRunner } from 'src/utils/gql-test-runner.utils';
-import { GraphQLErrorCode } from 'src/utils/gql-error-code.enum';
+import { User } from '../entities/user.entity';
 
 describe('UserResolver (e2e)', () => {
   let app: INestApplication;
+  let firstUser: User;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -28,7 +29,7 @@ describe('UserResolver (e2e)', () => {
   });
 
   describe('findAll()', () => {
-    it('should return some users', async () => {
+    it('should return an array of users', async () => {
       const query = gql`
         query {
           users {
@@ -36,7 +37,34 @@ describe('UserResolver (e2e)', () => {
           }
         }
       `;
-      GqlTestRunner.sendGqlRequest(app.getHttpServer(), query);
+
+      const res = await GqlTestRunner.sendGqlRequest(
+        app.getHttpServer(),
+        query,
+      );
+      const users = res.body?.data?.users;
+      firstUser = users?.[0];
+
+      expect(users).toBeDefined();
+      expect(users?.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should return some users', async () => {
+      const query = gql`
+        query {
+          user(id: ${firstUser?.id}) {
+            id
+          }
+        }
+      `;
+      const res = await GqlTestRunner.sendGqlRequest(
+        app.getHttpServer(),
+        query,
+      );
+
+      expect(res.body?.data?.user).toBeDefined();
     });
   });
 });
