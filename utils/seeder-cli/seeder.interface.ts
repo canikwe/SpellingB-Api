827983d8219a835b1f33e1e4d9d2067833bd81d2
canikwe <c.anikwe@gmail.com>
2021-03-06 19:16:59 -0500

    🧹 remove remaining typeorm files

--- utils/seeder-cli/seeder.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Connection } from 'typeorm';
-
-export interface ISeeder {
-  name: string;
-  timeStamp: number;
-  up: (connection: Connection) => Promise<number>;
-  down: (connection: Connection) => Promise<void>;
-}
