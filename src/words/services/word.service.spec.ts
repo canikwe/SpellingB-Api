d61dffb0d90f0bd7b076967ac3afed10ba6803f2
canikwe <c.anikwe@gmail.com>
2021-03-07 08:10:19 -0500

    🧪 word module unit tests are fixed

--- src/words/services/word.service.spec.ts
+++ src/words/services/word.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WordService } from './word.service';
+import { WordRepository } from '../repositories/word.repository';
+import { createSpyFromClass } from '../../utils/unit-tests/create-spy-from-class';
 
 describe('WordService', () => {
   let service: WordService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WordService],
+      providers: [
+        WordService,
+        {
+          provide: WordRepository,
+          useValue: createSpyFromClass(WordRepository),
+        },
+      ],
     }).compile();
 
     service = module.get<WordService>(WordService);
