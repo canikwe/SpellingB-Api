20fb134f1f2bcd4562a539bcf0bca393adbdefaf
canikwe <c.anikwe@gmail.com>
2021-03-06 20:15:44 -0500

    🐛 fix word builder bug

--- prisma/seed-helpers/build-words.ts
+++ prisma/seed-helpers/build-words.ts
@@ -2,10 +2,7 @@ import { CreateWordInput } from 'src/words/dto/create-word.input';
 const faker = require('faker');
 
 export const buildWords = (): CreateWordInput[] => {
-  const wordsArray = new Array(100).fill(undefined);
-  const wordsSet: Set<CreateWordInput> = new Set(wordsArray);
-
-  return [...wordsSet].reduce((acc: CreateWordInput[]) => {
+  return new Array(100).fill(undefined).reduce((acc: CreateWordInput[]) => {
     return acc.concat({ name: faker.random.word().toLowerCase() });
   }, []);
 };
