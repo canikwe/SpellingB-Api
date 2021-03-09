605845e3b382c206a2992f94727375cb191871b5
canikwe <c.anikwe@gmail.com>
2021-03-09 00:15:14 -0500

    🧪 base unit tests are working

--- src/_base/repositories/base.repository.spec.ts
+++ src/_base/repositories/base.repository.spec.ts
@@ -5,6 +5,7 @@ import { BaseRepository } from './base.repository';
 
 describe('BaseRepository', () => {
   let repository: BaseRepository;
+  let prismaSpy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,16 +13,37 @@ describe('BaseRepository', () => {
         BaseRepository,
         {
           provide: PrismaService,
-          useValue: createPrismaSpy('users'),
+          useValue: createPrismaSpy('foos'),
         },
       ],
     }).compile();
     repository = module.get(BaseRepository);
+    prismaSpy = module.get(PrismaService);
   });
 
   describe('Initialization', () => {
     it('should be defined', () => {
       expect(repository).toBeDefined();
+    });
+  });
+
+  describe('find()', () => {
+    beforeEach(() => {
+      (prismaSpy.foos.findMany as jest.Mock).mockResolvedValue([{}]);
+    });
+
+    it('should return array of foos', () => {
+      return expect(repository.find('foos')).resolves.toEqual([{}]);
+    });
+  });
+
+  describe('findOne()', () => {
+    beforeEach(() => {
+      (prismaSpy.foos.findFirst as jest.Mock).mockResolvedValue({});
+    });
+
+    it('should return a foo', async () => {
+      return expect(repository.findOne(1, 'foos')).resolves.toEqual({});
     });
   });
 });
