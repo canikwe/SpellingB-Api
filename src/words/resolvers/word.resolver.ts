90a2f226c074cdcc1015fa6cc0c3a5350f13d8cb
canikwe <c.anikwe@gmail.com>
2021-03-11 01:21:03 -0500

    🛠 removeunneded query code

--- src/words/resolvers/word.resolver.ts
+++ src/words/resolvers/word.resolver.ts
@@ -2,11 +2,11 @@ import { Resolver } from '@nestjs/graphql';
 
 import { BaseResolver } from 'src/_base/resolvers/base.resolver';
 import { Word } from '../entities/word.entity';
-import { WordService } from '../services/word.service';
+import { BaseService } from '../../_base/services/base.service';
 
 @Resolver(() => Word)
 export class WordResolver extends BaseResolver(Word) {
-  constructor(private readonly wordService: WordService) {
-    super(wordService);
+  constructor(private readonly baseService: BaseService) {
+    super(baseService);
   }
 }
--- src/words/word.module.ts
+++ src/words/word.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from '../_base/services/prisma.service';
 import { WordRepository } from './repositories/word.repository';
 import { WordResolver } from './resolvers/word.resolver';
 import { WordService } from './services/word.service';
+import { BaseModule } from '../_base/base.module';
 
 @Module({
   providers: [WordResolver, WordService, WordRepository, PrismaService],
-  imports: [],
+  imports: [BaseModule],
 })
 export class WordModule {}
