7ff4ae80756277ab0c54ce5ffc7228a92f320b54
canikwe <c.anikwe@gmail.com>
2021-11-11 17:37:43 -0500

    rename tests for consistency

---
+++ src/words/e2e-tests/word-query.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../../app.module';
+import { gql } from 'apollo-server-express';
+import { GqlTestRunner } from '../../../test/e2e-tests';
+import { Word } from 'src/@generated/prisma-nestjs-graphql/_models/word.model';
+
+describe('Word Query (e2e)', () => {
+  let app: INestApplication;
+  let word: Word;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Initialization', () => {
+    it('should be defined', () => {
+      expect(app).toBeDefined();
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should return an array of words', async () => {
+      const query = gql`
+        query {
+          Words {
+            id
+          }
+        }
+      `;
+
+      const res = await GqlTestRunner.sendGqlRequest(
+        app.getHttpServer(),
+        query,
+      );
+      const words = res.body?.data?.Words;
+      word = words?.[0];
+
+      expect(words).toBeDefined();
+      expect(words?.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should return a word', async () => {
+      const query = gql`
+        query {
+          Word(id: ${word?.id}) {
+            id
+          }
+        }
+      `;
+      const res = await GqlTestRunner.sendGqlRequest(
+        app.getHttpServer(),
+        query,
+      );
+
+      expect(res.body?.data?.Word).toBeDefined();
+    });
+  });
+});
