9332285f0b3a6045f2df6d6166e43dcb40882e1a
canikwe <c.anikwe@gmail.com>
2021-11-11 11:45:34 -0500

    refactor seeds

--- prisma/seed-helpers/build-decks.ts
+++ prisma/seed-helpers/build-decks.ts
@@ -1,8 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const faker = require('faker');
 import { User } from 'src/@generated/prisma-nestjs-graphql/_models/user.model';
+import { PrismaClient } from '.prisma/client';
+import { Deck } from 'src/@generated/prisma-nestjs-graphql/_models/deck.model';
 
-export const buildDecks = (user: User): any[] => {
+export const buildDecks = (
+  prisma: PrismaClient,
+  user: User,
+): Promise<Deck>[] => {
+  return buildDecksList(user).map(async (data) => {
+    return await prisma.deck.create({
+      data,
+    });
+  });
+};
+
+const buildDecksList = (user: User): any[] => {
   return new Array(2).fill(undefined).reduce((acc: any[]) => {
     return acc.concat({
       title: faker.random.words(3),
--- prisma/seed-helpers/build-users.ts
+++ prisma/seed-helpers/build-users.ts
@@ -1,8 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+import { User } from 'src/@generated/prisma-nestjs-graphql/_models/user.model';
 import { CreateUserInput } from 'src/users/dto/create-user.input';
+import { PrismaClient } from '@prisma/client';
 const faker = require('faker');
 
-export const buildUsers = (): CreateUserInput[] => {
+export const buildUsers = (prisma: PrismaClient): Promise<User>[] => {
+  return buildUsersList().map(async (data) => {
+    return await prisma.user.create({
+      data,
+    });
+  });
+};
+
+const buildUsersList = (): CreateUserInput[] => {
   return new Array(1).fill(undefined).reduce((acc: CreateUserInput[]) => {
     return acc.concat({
       firstName: faker.name.firstName(),
